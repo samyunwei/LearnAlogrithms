17433e178535c0b903b35c340a6ed1e954af7f90
samyunwei <259375702@qq.com>
2016-06-29 00:31:41 +0800

    add somefunc

--- CAlogrithms/alogrithms/Vector.cpp
+++ CAlogrithms/alogrithms/Vector.cpp
@@ -8,28 +8,6 @@
 
 #include "Vector.h"
 
-//扩容两倍
-template<typename T>
-void Vector<T>::expand(void)
-{
-    if(m_size < m_capacity)
-    {
-        return;
-    }
-    if(m_capacity < DEFAULT_CAPACITY)
-    {
-        m_capacity = DEFAULT_CAPACITY;
-    }
-    
-    T* oldElem = m_elem;
-    m_elem = new T[m_capacity << 2];
-    for(int i = 0;i<m_size;i++)
-    {
-        m_elem[i] = oldElem[i];
-    }
-    delete []oldElem;
-}
-
 
 //构造函数
 template<typename T>
@@ -69,6 +47,70 @@ Vector<T>& Vector<T>::operator=(Vector<T>const& V)
     return *this;
 }
 
+//重载[]运算符方便访问
+template <typename T>
+T& Vector<T>::operator[](Rank r)const
+{
+    return m_elem[r];
+}
+
+
+//交换方便日后操作
+template <typename T>
+void Vector<T>::swap(T& T1,T& T2)
+{
+    T* temp = &T1;
+    &T1 = &T2;
+    &T2 = temp;
+}
+
+
+//扩容两倍
+template<typename T>
+void Vector<T>::expand(void)
+{
+    if(m_size < m_capacity)
+    {
+        return;
+    }
+    if(m_capacity < DEFAULT_CAPACITY)
+    {
+        m_capacity = DEFAULT_CAPACITY;
+    }
+    
+    T* oldElem = m_elem;
+    m_elem = new T[m_capacity << 2];
+    for(int i = 0;i<m_size;i++)
+    {
+        m_elem[i] = oldElem[i];
+    }
+    delete []oldElem;
+}
+
+
+//缩容至25% - 50%,使复杂大量算法提高效率
+template<typename T>
+void Vector<T>::shrink(void)
+{
+    if(m_capacity < DEFAULT_CAPACITY << 1)
+    {
+        return;
+    }
+    
+    if(m_size << 2 > m_capacity)
+    {
+        return;
+    }
+    
+    T* oldElem = m_elem;
+    m_elem =  new T[m_capacity >>= 1];
+    for(int i = 0;i <m_size;i++)
+    {
+        m_elem[i] = oldElem[i];
+    }
+    delete [] oldElem;
+}
+
 
 //从另一Vector拷贝
 template<typename T>
@@ -80,4 +122,26 @@ void Vector<T>::copyfrom(T const *A,Rank low,Rank high)
     {
         m_elem[m_size++] = A[low++];
     }
+}
+
+//置乱一个区间
+template<typename T>
+void Vector<T>::unsort(Rank low,Rank high)
+{
+    T* V = m_elem + low;
+    for(Rank i = high - low; i>0 ;i--)
+    {
+        swap(V[i-1],V[rand() % i]);
+    }
+}
+
+//返回查找到的元素的置最大者若无值则返回-1
+template<typename T>
+Rank Vector<T>::find(T const&e,Rank low,Rank high)const
+{
+    while((low < high)&& (e != m_elem[high]))
+    {
+        high--;
+    }
+    return high;
 }
--- CAlogrithms/alogrithms/Vector.h
+++ CAlogrithms/alogrithms/Vector.h
@@ -12,18 +12,36 @@
 typedef int Rank;
 #define DEFAULT_CAPACITY 3
 #include <stdio.h>
+#include <stdlib.h>
 template<typename T > class Vector
 {
 protected:
-    Rank m_size;
-    int m_capacity;
-    T* m_elem;
-    
+    Rank m_size; //向量大小
+    int m_capacity;//向量容量
+    T* m_elem;//向量指针
+    //构造函数
     Vector(int c = DEFAULT_CAPACITY,int s = 0,T v = 0);
+    
+    //
     ~Vector();
+    
+    //运算符重载
     Vector<T> & operator=(Vector<T>const&);
+    T& operator[](Rank r) const;
+    
+    //交换
+    void swap(T& T1,T& T2);
+    //扩容
     void expand(void);//expand double size;
+    //缩容
+    void shrink(void);
+    //拷贝
     void copyfrom(T const *A,Rank low,Rank high);
+    //置乱
+    void unsort(Rank low,Rank high);
+    //无序向量查找
+    Rank find(T const&e,Rank low,Rank high)const;
+
     
 };
 #endif /* Vector_h */
