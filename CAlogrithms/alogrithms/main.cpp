0c1dd9837e6ecdb63037e3d022c1b593115ee98f
samyunwei <259375702@qq.com>
2016-06-28 23:41:40 +0800

    addd xcodepro

---
+++ CAlogrithms/alogrithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CAlogrithms
+//
+//  Created by sam on 16/6/28.
+//  Copyright © 2016年 sam. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
