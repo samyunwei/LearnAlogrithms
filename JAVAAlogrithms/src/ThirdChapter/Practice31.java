4da8687b50db67c39378f5cd1b05942c3b432ddf
samyunwei <259375702@qq.com>
2017-01-31 23:43:50 +0800

    add practice31

---
+++ JAVAAlogrithms/src/ThirdChapter/Practice31.java
@@ -0,0 +1,17 @@
+package ThirdChapter;
+
+/**
+ * Author:Sam
+ * Mail:samyunwei@163.com
+ * Create Time: 2017/1/31
+ */
+public class Practice31 {
+    public static void main(String[] args) {
+
+    }
+
+    public void averageMarks()
+    {
+
+    }
+}
