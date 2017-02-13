ab7d4916fb6db5ae15beda3e6bbf760c05c0ef67
Sam <259375702@qq.com>
2017-02-13 20:32:03 +0800

    add BST

---
+++ JAVAAlogrithms/src/ThirdChapter/BST.java
@@ -0,0 +1,89 @@
+package ThirdChapter;
+
+/**
+ * Author:Sam
+ * Mail:samyunwei@163.com
+ * Create Time: 2017/2/13
+ */
+public class BST<Key extends Comparable<Key>, Value> {
+    private Node root;
+    private int totalcounter;
+
+    public BST() {
+        totalcounter = 0;
+    }
+
+    public int getTotalcount() {
+        return totalcounter;
+    }
+
+    public void clearcounter() {
+        totalcounter = 0;
+    }
+
+
+    private class Node {
+        private Key key;
+        private Value val;
+        private Node left, right;
+        private int N;
+
+        public Node(Key key, Value val, int n) {
+            this.key = key;
+            this.val = val;
+            N = n;
+        }
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        } else {
+            return x.N;
+        }
+    }
+
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    public Value get(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        totalcounter++;
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
+        } else {
+            return x.val;
+        }
+    }
+
+    public void put(Key key, Value val) {
+        root = put(root, key, val);
+    }
+
+    public Node put(Node x, Key key, Value val) {
+        if (x == null) {
+            return new Node(key, val, 1);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, val);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, val);
+        } else {
+            x.val = val;
+        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+}
