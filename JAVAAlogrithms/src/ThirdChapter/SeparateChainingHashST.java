2a0f5cbf337b66971e75b9472346d5fe8a0c22f3
Sam <259375702@qq.com>
2017-03-21 00:12:05 +0800

    add separateChainHashST

---
+++ JAVAAlogrithms/src/ThirdChapter/SeparateChainingHashST.java
@@ -0,0 +1,41 @@
+package ThirdChapter;
+
+/**
+ * Author:Sam
+ * Mail:samyunwei@163.com
+ * Create Time: 2017/3/20
+ */
+public class SeparateChainingHashST<Key,Value> {
+    private int N;
+    private int M;
+    private SequentialSearchST<Key,Value>[]st;
+
+    public SeparateChainingHashST() {
+        this(997);
+    }
+
+    public SeparateChainingHashST(int M){
+        this.M = M;
+        st = (SequentialSearchST<Key,Value>[]) new SequentialSearchST[M];
+        for(int i = 0;i < M ;i++){
+            st[i] = new SequentialSearchST<Key,Value>();
+        }
+    }
+
+    private int hash(Key key){
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    public Value get(Key key){
+        return st[hash(key)].get(key);
+    }
+
+    public void put(Key key,Value val){
+        st[hash(key)].put(key,val);
+    }
+
+    
+
+
+
+}
