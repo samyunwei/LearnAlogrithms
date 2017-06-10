8e1bcdfacaa2d66ab8cc1b6344335f101e75b78e
Sam <259375702@qq.com>
2017-06-10 14:40:46 +0800

    add DepthFirstSearch

--- JAVAAlogrithms/src/FourthChapter/TestSearch.java
+++ JAVAAlogrithms/src/FourthChapter/TestSearch.java
@@ -14,15 +14,15 @@ public class TestSearch {
     public static void main(String[] args) {
         Graph G = new Graph(new In(args[0]));
         int s = Integer.parseInt(args[1]);
-        DepthFirstSearch search = new DepthFirstSearch(G,s);
-        for(int v = 0;v < G.V();v++){
-            if(search.marked(v)){
+        DepthFirstSearch search = new DepthFirstSearch(G, s);
+        for (int v = 0; v < G.V(); v++) {
+            if (search.marked(v)) {
                 StdOut.println(v + " ");
             }
         }
         StdOut.println();
 
-        if(search.count() != G.V()){
+        if (search.count() != G.V()) {
             StdOut.print("NOT ");
         }
         StdOut.println("connected");
